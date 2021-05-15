478dd2c6265b5698008c1d15e033863572d060eb
hyooosong <gythd1998@gmail.com>
2021-05-15 14:11:05 +0900

    [layout] Base setting for Creating Profile

---
+++ app/src/main/java/find/CreateProfileActivity.kt
@@ -0,0 +1,12 @@
+package find
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import find.ui.R
+
+class CreateProfileActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_profile)
+    }
+}
