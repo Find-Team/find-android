4079e2e6d6f6b6f940b6b40f971e0635e1d925f5
hyooosong <gythd1998@gmail.com>
2021-05-19 01:40:59 +0900

    [refactor] Simplify Profile Binding #64

--- app/src/main/java/find/ui/ui/profile/ProfileBinding.kt
+++ app/src/main/java/find/ui/ui/profile/ProfileBinding.kt
@@ -1,25 +1,26 @@
 package find.ui.ui.profile
 
+import android.annotation.SuppressLint
 import android.graphics.Color
-import android.text.TextWatcher
-import android.widget.EditText
 import android.widget.TextView
 import androidx.databinding.BindingAdapter
+import find.ui.R
 
 object ProfileBinding {
     @BindingAdapter("changeColor")
     @JvmStatic
     fun changeColor(text: TextView, content: String?) {
         when (content?.length) {
-            in 11..499 -> text.setTextColor(Color.BLUE)
-            500 -> text.setTextColor(Color.RED)
-            else -> text.setTextColor(Color.parseColor("#bdbdbd"))
+            in 10..499 -> text.setTextColor(Color.BLUE)
+            500 -> text.setTextColor(text.context.getColor(R.color.red_eb57))
+            else -> text.setTextColor(text.context.getColor(R.color.gray_bd))
         }
     }
 
-    @BindingAdapter("textChangedListener")
+    @SuppressLint("SetTextI18n")
+    @BindingAdapter("setTextLength")
     @JvmStatic
-    fun textChangedListener(editText: EditText, textWatcher: TextWatcher?) {
-        editText.addTextChangedListener(textWatcher)
+    fun setTextLength(text: TextView, editText: String?) {
+        text.text = "${editText?.length}/500"
     }
 }
--- app/src/main/java/find/ui/ui/profile/ProfileViewModel.kt
+++ app/src/main/java/find/ui/ui/profile/ProfileViewModel.kt
@@ -1,7 +1,5 @@
 package find.ui.ui.profile
 
-import android.text.Editable
-import android.text.TextWatcher
 import androidx.databinding.ObservableField
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
@@ -12,18 +10,9 @@ class ProfileViewModel : ViewModel() {
     val introLength: LiveData<String>
         get() = _introLength
 
-    init {
-        _introLength.value = "0/500"
-    }
-
     val introContent = ObservableField<String>()
 
-    val watcher = object : TextWatcher {
-        override fun beforeTextChanged(s: CharSequence, start: Int, count: Int, after: Int) {}
-        override fun onTextChanged(s: CharSequence, start: Int, before: Int, count: Int) {
-            _introLength.value = "${introContent.get()?.length}/500"
-        }
-
-        override fun afterTextChanged(s: Editable) {}
+    init {
+        _introLength.value = "0/500"
     }
 }
