1dd5502bbc7ac7da87b7f006aae7be9eba65e494
kym1924 <kym1924@naver.com>
2021-05-18 11:55:57 +0900

    [layout] SelectFragment #59

---
+++ app/src/main/java/find/ui/ui/select/SelectFragment.kt
@@ -0,0 +1,18 @@
+package find.ui.ui.select
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import find.ui.R
+
+class SelectFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_select, container, false)
+    }
+}
