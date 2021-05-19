948aab83e7e57b3c534a55c8a415b82516eb4313
hyooosong <gythd1998@gmail.com>
2021-05-20 00:47:17 +0900

    [refactor] Rearrange the Package #70

--- app/src/main/java/find/ui/ui/mypage/IntroductionActivity.kt
+++ app/src/main/java/find/ui/ui/mypage/IntroductionActivity.kt
@@ -1,9 +1,10 @@
-package find.ui.ui.profile
+package find.ui.ui.mypage
 
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import find.ui.databinding.ActivityIntroductionBinding
+import find.ui.ui.profile.ProfileViewModel
 
 class IntroductionActivity : AppCompatActivity() {
     private lateinit var binding: ActivityIntroductionBinding
