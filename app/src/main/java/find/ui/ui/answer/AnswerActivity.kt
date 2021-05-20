edc85e2cc6727e7f879fad030a3e752d3d4c3e95
kym1924 <kym1924@naver.com>
2021-05-21 01:02:28 +0900

    [layout] AnswerActivity #82

---
+++ app/src/main/java/find/ui/ui/answer/AnswerActivity.kt
@@ -0,0 +1,12 @@
+package find.ui.ui.answer
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import find.ui.R
+
+class AnswerActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_answer)
+    }
+}
