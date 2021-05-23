44841d56e8ba5cc5659b5160e985d4bdd6bdc0ca
kym1924 <kym1924@naver.com>
2021-05-23 15:15:19 +0900

    [feature] Setting QuestionAdapter #96

---
+++ app/src/main/java/find/ui/ui/question/QuestionAdapter.kt
@@ -0,0 +1,39 @@
+package find.ui.ui.question
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import find.ui.BR
+import find.ui.databinding.ItemQuestionBinding
+
+class QuestionAdapter :
+    ListAdapter<Question, QuestionAdapter.QuestionViewHolder>(QuestionDiffUtil()) {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        QuestionViewHolder(
+            ItemQuestionBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    override fun onBindViewHolder(holder: QuestionViewHolder, position: Int) =
+        holder.bind(getItem(position))
+
+    class QuestionViewHolder(private val binding: ItemQuestionBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(question: Question) {
+            binding.setVariable(BR.question, question)
+        }
+    }
+
+    private class QuestionDiffUtil : DiffUtil.ItemCallback<Question>() {
+        override fun areItemsTheSame(oldItem: Question, newItem: Question) =
+            oldItem.question == newItem.question
+
+        override fun areContentsTheSame(oldItem: Question, newItem: Question) = oldItem == newItem
+    }
+}
