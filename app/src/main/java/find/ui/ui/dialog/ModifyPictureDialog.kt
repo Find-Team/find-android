fec9d6bf3604dde37050d4ca6a2bf0f3914f10fd
hyooosong <gythd1998@gmail.com>
2021-05-28 16:14:42 +0900

    [layout] Modify Picture Dialog #139

---
+++ app/src/main/java/find/ui/ui/dialog/ModifyPictureDialog.kt
@@ -0,0 +1,70 @@
+package find.ui.ui.dialog
+
+import android.Manifest
+import android.app.AlertDialog
+import android.app.Dialog
+import android.os.Bundle
+import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.fragment.app.DialogFragment
+import find.ui.R
+import find.ui.databinding.DialogModifyPictureBinding
+import find.ui.ui.mypage.MyPageActivity
+import find.ui.util.autoCleared
+
+class ModifyPictureDialog : DialogFragment() {
+    var binding by autoCleared<DialogModifyPictureBinding>()
+
+    private val requestPermissionLauncher = registerForActivityResult(
+        ActivityResultContracts.RequestPermission()
+    ) { isGranted ->
+        if (isGranted) {
+            (requireActivity() as MyPageActivity).selectImage()
+        } else {
+            Toast.makeText(requireContext(), "권한을 승인해주세요", Toast.LENGTH_SHORT).show()
+        }
+    }
+
+    override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
+        binding = DialogModifyPictureBinding.inflate(requireActivity().layoutInflater)
+        return activity?.let {
+            val dialog = AlertDialog.Builder(it).create()
+            dialog.setView(binding.root)
+            onClickCancel()
+            onClickGet()
+            dialog
+        } ?: throw IllegalStateException()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        requireNotNull(dialog).apply {
+            requireNotNull(window).apply {
+                setLayout(
+                    (resources.displayMetrics.widthPixels * 0.9).toInt(),
+                    ViewGroup.LayoutParams.WRAP_CONTENT
+                )
+                setBackgroundDrawableResource(R.drawable.border_white_fill_round_10)
+            }
+            setCancelable(false)
+        }
+    }
+
+    private fun onClickCancel() {
+        binding.tvPictureCancel.setOnClickListener {
+            dismiss()
+        }
+    }
+
+    private fun onClickGet() {
+        binding.tvPictureGet.setOnClickListener {
+            requestPermissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            dismiss()
+        }
+    }
+
+    companion object {
+        const val PICTURE_TAG = "MODIFY_PICTURE_DIALOG"
+    }
+}
